16d3461a1a529eeddb9faa1642cb9acbb09f5d8f
beeplaced <you@example.com>
2024-01-12 13:17:59 +0100

    1.1.0 upload as binary

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ module.exports = class {
             mb = 8
         } = meta;
 
+        this.fileLimit = mb * 1024 * 1024
         this.uploadPath = uploadPath
 
         const storage = multer.diskStorage({
@@ -32,7 +33,7 @@ module.exports = class {
         this.upload = multer({
             storage,
             limits: {
-                fileSize: mb * 1024 * 1024
+                fileSize: this.fileLimit
             },
             fileFilter: (req, file, cb) => {
                 if (allowedMimeTypes.includes(file.mimetype)) {
@@ -88,6 +89,37 @@ module.exports = class {
                 })
             })
         } catch (e) { console.log(e) }
+    }
+
+    uploadFileChunk = async (req, res) => {//upload as binary
+        const body = [];
+        req.on('data', chunk => {
+            for (let offset = 0; offset < chunk.byteLength; offset += fileLimit) {
+                const chunkEnd = Math.min(offset + fileLimit, chunk.byteLength);
+                const newChunk = chunk.slice(offset, chunkEnd);
+                body.push(newChunk);
+            }
+            body.push(chunk)
+        });
+
+        req.on('end', async () => {
+            try {
+                const filemeta = {}
+                filemeta.buffer = Buffer.concat(body);
+                if (req.headers['file-name']) filemeta.originalname = req.headers['file-name']
+                filemeta.size = filemeta.buffer.length
+                filemeta.sortsize = filesize(filemeta.size)
+                filemeta.sha = await this.calculateSHA256(filemeta.buffer)
+                res.status(200).json(filemeta);
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({ data: { status: 500, error: 'An error occurred during file storage.' } });
+            }
+        });
+
+        req.on('error', err => {
+            res.status(500).json({ data: { status: 500, error: 'An error occurred while uploading files.' } });
+        });
     }
 
 }
